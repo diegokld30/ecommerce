3b020f512e2b56ff29cd96f418d37b80b45250b0
diegokld30 <dfercalderon@poligran.edu.co>
2025-04-20 20:26:00 -0500

    Se genero la logica para manejar errores basicos

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -7,6 +7,8 @@ import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
+
+  private readonly logger = new Logger('ProductsService');
 
   constructor(
     @InjectRepository(Product)
@@ -25,8 +27,7 @@ export class ProductsService {
       return product;
 
     } catch (error) {
-      console.log(error)
-      throw new InternalServerErrorException('Aiuda!!!')
+      this.handleDBExceptions(error);
     }
   }
 
@@ -44,5 +45,16 @@ export class ProductsService {
 
   remove(id: number) {
     return `This action removes a #${id} product`;
+  }
+
+
+  private handleDBExceptions( error:any ){
+          
+    if ( error.code === '23505'){
+      throw new BadRequestException(error.detail)
+    }
+    this.logger.error(error)
+    throw new InternalServerErrorException('Error desconocido, revise el log del servidor.')
+
   }
 }
